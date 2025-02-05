8910119dad1d37d26094c57de95314e81af3303f
JungleHunterPredator <kingsley.yam.zhen@gmail.com>
2025-02-05 18:54:24 +0800

    Home Update (5/2)

--- Kingsley/Home/script.js
+++ Kingsley/Home/script.js
@@ -8,3 +8,12 @@ hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
     searchBar.classList.toggle('active');
 });
+
+const infoBoxes = document.querySelectorAll('.info-box');
+
+infoBoxes.forEach(box => {
+    box.addEventListener('click', () => {
+        const link = box.querySelector('h2 a').href; // Get the link from the title
+        window.location.href = link; // Redirect to the link
+    });
+});
